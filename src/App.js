de3752e3c72c50f048ee8a6d57cc6a7e108171e7
Amadu Askofare <amadouaskofare@gmail.com>
2020-02-19 21:45:16 +0400

    uploading files

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import { CardList } from './components/cardList/cardList.component'
-import { SearchBox } from './components/searchBox/search-box.component'
-import './App.css';
+import React from "react";
+import { CardList } from "./components/cardList/cardList.component";
+import { SearchBox } from "./components/searchBox/search-box.component";
+import "./App.css";
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       monsters: [],
-      searchField: ''
-    }
+      searchField: ""
+    };
   }
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
   handleChange = event => {
-    this.setState({searchField : event.target.value})
-  }
-  render(){
-    const {monsters, searchField} = this.state
-    const filteredMonster = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+    this.setState({ searchField: event.target.value });
+  };
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonster = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-      <SearchBox
-        placeholder = "search monster"
-        handleChange = {this.handleChange}
-      />
-        <CardList monsters = {filteredMonster}/>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monster"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonster} />
       </div>
     );
   }
