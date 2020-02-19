79d0e33c57e966731188d6de69a782ac6b9f5556
Amadu Askofare <amadouaskofare@gmail.com>
2020-02-19 22:46:13 +0400

    Updates

---
+++ precache-manifest.aacdbb6242cec75c6217c697a275aa89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5fe7aa6518768808f907ecb350654c",
+    "url": "/robotdecs/index.html"
+  },
+  {
+    "revision": "fe1bbe12086aaa5d345c",
+    "url": "/robotdecs/static/css/main.f12621d0.chunk.css"
+  },
+  {
+    "revision": "8ecbc468b3a10bf4e256",
+    "url": "/robotdecs/static/js/2.c0f1f819.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robotdecs/static/js/2.c0f1f819.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe1bbe12086aaa5d345c",
+    "url": "/robotdecs/static/js/main.9a301047.chunk.js"
+  },
+  {
+    "revision": "f975d74f1b1c8633468f",
+    "url": "/robotdecs/static/js/runtime-main.be4e50c3.js"
+  }
+]);
